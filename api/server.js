a9aed6d2559e43c0fb7ef21bfe98576ef7264b1f
cyamos2 <cyamos2@yahoo.com>
2021-09-03 11:18:32 -0400

    done for now

--- api/server.js
+++ api/server.js
@@ -1,2 +1,13 @@
 // implement your server here
 // require your posts router and connect it here
+const express = require('express');
+ const server = express();
+ const postRouter = require('./posts/posts-router.js');
+
+ server.use(express.json());
+ server.use("/api/posts", postRouter);
+
+ // require your posts router and connect it here
+
+
+ module.exports = server 
